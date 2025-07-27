3021aed01cbd2c6b1d6aa3b1130103a4ef1abe40
marpilip <mar.pilip@gmail.com>
2025-07-27 16:54:48 +0700

    Старт проекта

---
+++ src/main/java/bank/springpets/SpringpetsApplication.java
@@ -0,0 +1,13 @@
+package bank.springpets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringpetsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bank/springpets/SpringpetsApplicationTests.java
@@ -0,0 +1,13 @@
+package bank.springpets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
